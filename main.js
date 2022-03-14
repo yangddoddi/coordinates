112f798e3d72d4952939db781d677ee7c14089ee
yangddoddi <pud1251@gmail.com>
2022-03-14 13:07:11 +0900

    JS,CSS_retouch
    성능 개선

--- main.js
+++ main.js
@@ -1,19 +1,22 @@
-const pointer = document.querySelector(".pointer");
-const coordinates = document.querySelector(".coordinates");
-const verticalLine = document.querySelector(".verticalLine");
-const horizontaLine = document.querySelector(".horizontaLine");
+addEventListener("load", () => {
+  const pointer = document.querySelector(".pointer");
+  const coordinates = document.querySelector(".coordinates");
+  const verticalLine = document.querySelector(".verticalLine");
+  const horizontaLine = document.querySelector(".horizontaLine");
 
-function MovePointer(event) {
-  pointer.style.top = `${event.clientY}px`;
-  pointer.style.left = `${event.clientX}px`;
-  verticalLine.style.left = `${event.clientX}px`;
-  horizontaLine.style.top = `${event.clientY}px`;
-  coordinates.style.top = `${event.clientY}px`;
-  coordinates.style.left = `${event.clientX}px`;
-  coordinates.innerHTML = `${event.clientX}px, ${event.clientY}px`;
-}
+  function MovePointer(event) {
+    pointer.style.transform = `translate(${
+      event.clientX - pointer.getBoundingClientRect().width / 2
+    }px, ${event.clientY - pointer.getBoundingClientRect().height / 2}px)`;
+    verticalLine.style.transform = `translateX(${event.clientX}px)`;
+    horizontaLine.style.transform = `translateY(${event.clientY}px)`;
+    coordinates.style.transform = `translate(${event.clientX + 20}px,${
+      event.clientY
+    }px)`;
+    coordinates.innerHTML = `${event.clientX}px, ${event.clientY}px`;
+  }
 
-window.addEventListener("mousemove", (event) => {
-  MovePointer(event);
-  console.log(event);
+  window.addEventListener("mousemove", (event) => {
+    MovePointer(event);
+  });
 });
